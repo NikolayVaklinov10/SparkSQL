7f6b466c8587bad9a91dfba552124829f036cb4e
Nick <nikolayvaklinov@yahoo.com>
2019-07-22 16:07:03 +0100

    initial commit

---
+++ sparksql.py
@@ -0,0 +1,18 @@
+"""basic SparkSession"""
+
+
+from pyspark.sql import SparkSession
+
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+
+
+
+
+
+
